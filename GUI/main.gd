8a62ad9df2ac8e27dc896d9b727f4973e1a064dc
Miguel Castro Castro <m.castroc@udc.es>
2023-04-13 12:50:26 +0200

    Mock UI for easier debugging

--- GUI/main.gd
+++ GUI/main.gd
@@ -1,9 +1,85 @@
 extends Control
 
+var program_loaded: bool = false
+
+@onready
+var pipelinedWrapper: PipelinedWrapper = $PipelinedWrapper
+
+func _ready():
+	Globals.load_program_pressed.connect(self._on_load_program_pressed)
+	_setup_cpu_info()
+
+
+func _setup_cpu_info():
+	# add labels for special registers
+	var labels_text = ["Special Registers", "PC", "HI", "LO"]
+	for i in 4: #number of special registers so far + title
+		var label: Label = Label.new()
+		label.text = labels_text[i]
+		%SpecialRegistersVBoxContainer.add_child(label)
+	
+	# add labels for registers
+	labels_text = ["Integer Registers", "Single Precision FP Registers", "Double Precision FP Registers"]
+	var labels: Array[Label] = []
+	for i in 3: #number of register types
+		var label: Label = Label.new()
+		label.text = labels_text[i]
+		labels.append(label)
+	%IntegerRegistersVBoxContainer.add_child(labels[0])
+	%FloatRegistersVBoxContainer.add_child(labels[1])
+	%DoubleRegistersVBoxContainer.add_child(labels[2])
+	
+	for i in 32:
+		var label: Label = Label.new()
+		label.name = str(i)
+		var label2: Label = Label.new()
+		label2.name = str(i)
+		
+		%IntegerRegistersVBoxContainer.add_child(label)
+		%FloatRegistersVBoxContainer.add_child(label2)
+		
+		if i < 16:
+			label = Label.new()
+			label.name = str(i)
+			%DoubleRegistersVBoxContainer.add_child(label)
+
+
+
+func update_cpu_info():
+	#special registers labels
+	(%SpecialRegistersVBoxContainer.get_child(1) as Label).text = "PC: " + pipelinedWrapper.cpu_info["PCValue"]
+	(%SpecialRegistersVBoxContainer.get_child(2) as Label).text = "HI: " + pipelinedWrapper.cpu_info["HIValue"]
+	(%SpecialRegistersVBoxContainer.get_child(3) as Label).text = "LO: " + pipelinedWrapper.cpu_info["LOValue"]
+	
+	#registers
+	for i in 32:
+		#add 1 because of title row
+		(%IntegerRegistersVBoxContainer.get_child(i+1) as Label).text = pipelinedWrapper.cpu_info["Registers"]["iRegisters"][i]
+		(%FloatRegistersVBoxContainer.get_child(i+1) as Label).text = pipelinedWrapper.cpu_info["Registers"]["fRegisters"][i]
+		
+		if i < 16:
+			(%DoubleRegistersVBoxContainer.get_child(i+1) as Label).text = pipelinedWrapper.cpu_info["Registers"]["dRegisters"][i]
+
 
 func _on_load_program_pressed():
-	print($PipelinedWrapper.load_program("/home/mike/Desktop/SharedFolder/TFG/mips_sim/testdata/asm1.s"))
+	pipelinedWrapper.free()
+	pipelinedWrapper = PipelinedWrapper.new()
+	pipelinedWrapper.name = "PipelinedWrapper"
+	self.add_child(pipelinedWrapper)
+	program_loaded = $PipelinedWrapper.load_program("/home/mike/Desktop/SharedFolder/TFG/mips_sim/testdata/asm1.s")
+	if program_loaded and $PipelinedWrapper.is_ready():
+		%NextCycle.disabled = false
+		%RunProgram.disabled = false
 
 
 func _on_next_cycle_pressed():
-	$PipelinedWrapper.run_cycle()
+	if program_loaded and $PipelinedWrapper.is_ready():
+		$PipelinedWrapper.next_cycle()
+		update_cpu_info()
+
+
+func _on_run_program_pressed():
+	if program_loaded and $PipelinedWrapper.is_ready():
+		while $PipelinedWrapper.is_ready():
+			$PipelinedWrapper.next_cycle()
+		update_cpu_info()
--- src/PipelinedWrapper.h
+++ src/PipelinedWrapper.h
@@ -10,19 +10,34 @@
 #include <godot_cpp/classes/node.hpp>
 namespace godot {
 
+typedef struct {
+    godot::String PCValue;
+    godot::String LOValue;
+    godot::String HIValue;
+    godot::String cycles;
+
+} cpu_info_t;
+
 class PipelinedWrapper : public Node{
     GDCLASS(PipelinedWrapper, Node);
 protected:
     static void _bind_methods();
 public:
+    godot::Dictionary* cpu_info;
     std::unique_ptr<mips_sim::Cpu> cpu;
     std::shared_ptr<mips_sim::Memory> mem;
     PipelinedWrapper();
     ~PipelinedWrapper();
-    godot::String run_cycle();
+    godot::String next_cycle();
     godot::String init();
     
-    godot::String load_program(godot::String filename);
+    bool is_ready();
+    bool load_program(godot::String filename);
+
+    void set_cpu_info(godot::Dictionary p_cpu_info);
+    godot::Dictionary get_cpu_info();
+
+    void _update_cpu_info();
 };
 }
 #endif //TFG_PIPELINED_WRAPPER_H

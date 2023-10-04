a620c6552459ce8ef77241faef31a5b4673b8f75
Miguel Castro Castro <m.castroc@udc.es>
2023-10-04 12:57:39 +0200

    Update RegBank_info_window.gd

--- GUI/RegBank_info_window.gd
+++ GUI/RegBank_info_window.gd
@@ -1,10 +1,13 @@
 extends Window
 
-@onready var v_box_container = $VBoxContainer
-@onready var grid_container = $VBoxContainer/ScrollContainer/GridContainer
+@onready var v_box_container: VBoxContainer = $VBoxContainer
+@onready var grid_container: GridContainer = $VBoxContainer/ScrollContainer/GridContainer
+@onready var parent: MainComponent = get_parent()
 
 func _ready():
+	Globals.recenter_window.connect(_on_recenter_window)
 	_on_size_changed()
+	
 	var name_array: Array = PipelinedWrapper.get_register_names()
 	for name in name_array:
 		var name_label: Label = Label.new()
@@ -20,6 +23,10 @@ func _ready():
 		value_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 		grid_container.add_child(name_label)
 		grid_container.add_child(value_label)
+		
+	position.x = clamp(parent.global_position.x + parent.size.x/2 - size.x/2, 0, DisplayServer.window_get_size().x - size.x -10) # -10 window border default theme
+	position.y = clamp(parent.global_position.y + parent.size.y/2 - size.y/2, 40, DisplayServer.window_get_size().y - size.y -10)
+	title = parent.window_title
 
 
 func _on_size_changed():
@@ -30,7 +37,15 @@ func _on_size_changed():
 func add_info():
 	visible = true
 	for i in range(1, 64, 2):
-		grid_container.get_child(i).text = PipelinedWrapper.cpu_info["Registers"]["iRegisters"][ceil(i % 32)]
+		print(i/2)
+		var value: String = PipelinedWrapper.cpu_info["Registers"]["iRegisters"][i/2]
+		grid_container.get_child(i).text = value
+		if value == "00000000":
+			(grid_container.get_child(i) as Label).add_theme_color_override("font_color", Color.DIM_GRAY)
+			(grid_container.get_child(i-1) as Label).add_theme_color_override("font_color", Color.DIM_GRAY)
+		else:
+			(grid_container.get_child(i) as Label).remove_theme_color_override("font_color")
+			(grid_container.get_child(i-1) as Label).remove_theme_color_override("font_color")
 		
 
 
@@ -40,3 +55,8 @@ func _on_close_requested():
 
 func _on_focus_exited():
 	visible = false
+
+
+func _on_recenter_window():
+	position.x = clamp(parent.global_position.x + parent.size.x/2 - size.x/2, 0, DisplayServer.window_get_size().x - size.x -10) # -10 window border default theme
+	position.y = clamp(parent.global_position.y + parent.size.y/2 - size.y/2, 40, DisplayServer.window_get_size().y - size.y -10)

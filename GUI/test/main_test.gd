348d6376942d9081546af4ee2297637d71186ca1
Miguel Castro Castro <m.castroc@udc.es>
2023-11-10 15:33:08 +0100

    Update MipsSim, add tests sp/dp

--- GUI/test/main_test.gd
+++ GUI/test/main_test.gd
@@ -8,11 +8,31 @@ extends GdUnitTestSuite
 const __source = 'res://main.gd'
 
 
-func test_main() -> void:
+func test_single_precision() -> void:
+	LineManager.stage_detail_path.clear()
+	LineManager.stage_register_path.clear()
 	var runner := scene_runner("res://main.tscn")
-	runner.invoke("_on_load_program_pressed", "testdata/testFPU2.s")
+	runner.invoke("_on_load_program_pressed", "testdata/testFPU3.s")
 	runner.invoke("_on_run_program_pressed")
+	
 	var fp_as_float = PipelinedWrapper.get_fp_register_values_f()
+	# simple
+	assert_float(fp_as_float[0]).is_equal(4.5)
+	assert_float(fp_as_float[2]).is_equal(9.)
+	assert_float(fp_as_float[4]).is_equal(40.5)
+	assert_float(fp_as_float[6]).is_equal(9.)
+	assert_float(fp_as_float[8]).is_equal(4.5)
+	assert_float(fp_as_float[10]).is_equal(9.)
+
+
+func test_double_precision() -> void:
+	LineManager.stage_detail_path.clear()
+	LineManager.stage_register_path.clear()
+	var runner := scene_runner("res://main.tscn")
+	await runner.simulate_frames(10)
+	runner.invoke("_on_load_program_pressed", "testdata/testFPU2.s")
+	await runner.simulate_frames(10)
+	runner.invoke("_on_run_program_pressed")
 	var fp_as_double = PipelinedWrapper.get_fp_register_values_d() # d registers return 16 values
 	# double
 	assert_float(fp_as_double[0 / 2]).is_equal(4.5)
@@ -20,9 +40,3 @@ func test_main() -> void:
 	assert_float(fp_as_double[4 / 2]).is_equal(40.5)
 	assert_float(fp_as_double[6 / 2]).is_equal(9.)
 	assert_float(fp_as_double[8 / 2]).is_equal(4.5)
-	# simple
-	assert_float(fp_as_float[10]).is_equal(9.)
-	assert_float(fp_as_float[12]).is_equal(13.5)
-	assert_float(fp_as_float[14]).is_equal(40.5)
-	assert_float(fp_as_float[16]).is_equal(4.5)
-	assert_float(fp_as_float[18]).is_equal(4.5)
